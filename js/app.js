a50239fe00c6da0c2b6cd75a156b9b6d7a513960
Daniel Lasauskas <Mazinger_086@yahoo.com>
2023-03-23 10:50:01 -0300

    Updating and fixing loading bug

--- js/app.js
+++ js/app.js
@@ -1,18 +1,63 @@
 let adviceId = document.querySelector('#adviceId');
 let adviceText = document.querySelector('#adviceText');
-let spinner = document.querySelector('.spinner');
+const spinner = document.querySelector('.spinner');
+const dice = document.querySelector('.dice');
+const diceImg = document.querySelector('.dice img');
 
+
+dice.addEventListener('click', ()=>{ 
+  animateDice();
+  setTimeout(()=>{    
+    adviceText.style.display = 'flex';
+    adviceId.style.display = 'flex';
+    spinner.classList.add("hide");
+  }, 2000);
+   
+  callApi(); 
+  adviceText.style.display = 'none';
+  adviceId.style.display = 'none';
+  spinner.classList.remove("hide");
+})
 
 async function advices(){
   let response = await fetch('https://api.adviceslip.com/advice');
-  let data = await response.json();
+  let data = await response.json(); 
   return data.slip;
 }
 
 
-advices().then(data => { 
-	adviceId.innerText = `# ${data.id}`; 
-    spinner.style = "display:none";    
+function callApi(){  
+  advices()
+  .then(data => {  
+    adviceId.innerText = `# ${data.id}`;       
     adviceText.innerHTML = `&#8220;${data.advice}&#8221;`; 
-});
+  })  
+}
+
+function animateDice(){  
+  diceImg.classList.add("diceSpin");
+  setTimeout(() => {
+    diceImg.classList.remove("diceSpin");
+  }, 1000);
+}
+
+
+// function checkAdviceBox(){
+//   // if(adviceText.length == 0){
+//   //   console.log("Esta vacio");
+//   // } else {
+//   //   console.log("Esta lleno");
+//   // }
+//   if(adviceText.innerHTML.trim().length === 0) {
+//     console.log("Esta vacio");
+//     callApi();
+//   } else {
+//     console.log("Esta lleno");
+//     spinner.classList.add("hide");
+//   } 
+  
+// }
+
+// checkAdviceBox();
+
 
